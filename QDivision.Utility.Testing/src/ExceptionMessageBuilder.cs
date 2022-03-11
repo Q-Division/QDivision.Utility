0b1970fd6a9f464f93699b85201d16c58c14bc4f
Q_Division <Q-Division@users.noreply.github.com>
2022-03-11 15:04:53 +0000

    QDivision.Utility
    * Added QDivision.Utility.Testing
    * Rearranged project slightly

---
+++ QDivision.Utility.Testing/src/ExceptionMessageBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QDivision.Utility.Testing
+{
+	/// <summary>
+	/// Builds exception messages for testing purposes
+	/// </summary>
+	public static class ExceptionMessageBuilder
+	{
+		/// <summary>
+		/// Build ArgumentException message based on DotNetCore 3.1+ or otherwise
+		/// </summary>
+		/// <param name="suppliedmessage">Message supplied to exception</param>
+		/// <param name="paramname">Name of parameter that triggered exception</param>
+		/// <returns>Message that comes out of exception</returns>
+		public static string BuildArgumentExceptionMessage(string suppliedmessage, string paramname)
+		{
+			return BuildArgumentExceptionMessageInternal(suppliedmessage, paramname);
+		}
+
+		/// <summary>
+		/// Build ArgumentNullException message based on DotNetCore 3.1+ or otherwise
+		/// </summary>
+		/// <param name="suppliedmessage">Message supplied to exception</param>
+		/// <param name="paramname">Name of parameter that triggered exception</param>
+		/// <returns>Message that comes out of exception</returns>
+		public static string BuildArgumentNullExceptionMessage(string suppliedmessage, string paramname)
+		{
+			return BuildArgumentExceptionMessageInternal(suppliedmessage, paramname);
+		}
+
+		#region Internal Methods
+		private static string BuildArgumentExceptionMessageInternal(string suppliedmessage, string paramname)
+		{
+			#if NETCOREAPP
+				var message = $"{suppliedmessage} (Parameter '{paramname}')";
+			#else
+				var message = $"{suppliedmessage}{Environment.NewLine}Parameter name: {paramname}";
+			#endif
+
+			return message;
+
+		}
+		#endregion
+	}
+}
---
+++ QDivision.Utility.Testing/test/ExceptionMessageBuilderTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using QDivision.Utility.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDivision.Utility.UnitTests.Testing
+{
+	[TestFixture]
+	public class ExceptionMessageBuilderTest
+	{
+		[Test]
+		public void BuildArgumentExceptionMessage()
+		{
+			//Setup
+			var message = "Some exception message";
+			var paramname = "someparam";
+
+			#if NETCOREAPP
+				var expectedmessage = $"{message} (Parameter '{paramname}')";
+			#else
+				var expectedmessage = $"{message}{Environment.NewLine}Parameter name: {paramname}";
+			#endif
+
+			//Run
+			var builtmessage = ExceptionMessageBuilder.BuildArgumentExceptionMessage(message, paramname);
+
+			//Check
+			Assert.That(builtmessage, Is.EqualTo(expectedmessage));
+		}
+	}
+}
